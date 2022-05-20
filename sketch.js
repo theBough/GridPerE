028eb6646f9821b07c0f6710466e5f0dbb8e3270
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-20 11:33:05 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(400, 400);
   createGrid();
-}
+} //end setup
 
 function draw() {
   background(220);
-  for(i=0 ; i< 10 ; i++){
-    grid[i].draw()
-  }
-  
-}
+  for (j = 0; j < 10; j++) {
+    for (i = 0; i < 10; i++) {
+      grid[j][i].draw();
+    } //end i loop
+  } //end j loop
+} //end draw
