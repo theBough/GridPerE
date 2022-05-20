e9f699ccedf0cf91ce5f148b62a3891ba72a1bda
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-20 13:26:31 -0500

    Added a draggable cube

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 function setup() {
   createCanvas(400, 400);
   createGrid();
+  createCube();
 } //end setup
 
 function draw() {
@@ -10,4 +11,9 @@ function draw() {
       grid[j][i].draw();
     } //end i loop
   } //end j loop
+  
+  c.draw();
+  if(c.isClicked){
+    c.locate(mouseX-15, mouseY - 15);
+  }//end if
 } //end draw
