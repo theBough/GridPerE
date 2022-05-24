4a17d6a7850608a44c381d96f2037adbc74e5787
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-24 14:35:04 -0500

    Update grid.js

--- grid.js
+++ grid.js
@@ -4,11 +4,12 @@ function createGrid() {
     grid[j] = []
     for (i = 0; i < 10; i++) {
       //this will make a row of 10
-      grid[j][i] = new Clickable(20 + i * 30, 20 + j *30);
+      grid[j][i] = new Clickable(30 + i * 30, 30 + j *30);
       grid[j][i].text = "";
       grid[j][i].cornerRadius = 0;
       grid[j][i].width = 30;
       grid[j][i].height = 30;
+      grid[j][i].text = grid[j][i].x
     } //end i loop
   } //end j loop
 } //end createGrid
