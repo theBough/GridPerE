2471858398b975f880e519ca3309f37fa89e9d04
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-24 14:58:45 -0500

    Made it stamp when it is dropped

--- cube.js
+++ cube.js
@@ -7,12 +7,24 @@ function createCube(){
   c.color = "purple";
   c.onPress = function(){
     c.isClicked = true;
-  }
+  }//end onPress
   c.onRelease = function(){
     c.isClicked = false;
     c.x = Math.floor(c.x / 30)*30
-    c.y = Math.floor(c.y / 30)*30
-    
-  }
-  
+    c.y = Math.floor(c.y / 30)*30 
+    findSpot(c.x, c.y);
+  } //end onrelease 
+}//end CreateCube
+
+function findSpot(thisX, thisY){
+  //this function will find out where the 
+  //block got dropped
+  for(j=0 ; j< 10 ; j++){
+    for(i=0 ; i<10 ; i++){
+      if(grid[j][i].x == thisX && grid[j][i].y == thisY){
+        grid[j][i].color = "yellow";
+        grid[j+1][i].color = "yellow";
+      }// end if
+    }//end i loop
+  }//end j loop
 }
