917a899005cfc0c7df61e9e851fe5f37e37881af
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-13 15:05:48 -0500

    Update bigCube.js

--- bigCube.js
+++ bigCube.js
@@ -1,38 +1,61 @@
 let bc;
-function createBigCube(){
-  bc = new Clickable(600,350);
+function createBigCube() {
+  bc = new Clickable(600, 350);
   bc.cornerRadius = 0;
-  bc.text = ""
-  bc.resize(90,90);
+  bc.text = "";
+  bc.resize(90, 90);
   bc.color = "blue";
-  bc.onPress = function(){
+  bc.onPress = function () {
     bc.isClicked = true;
-  }//end onPress
-  bc.onRelease = function(){
+  }; //end onPress
+  bc.onRelease = function () {
     bc.isClicked = false;
-    checkInGrid(bc)
-    bc.x = Math.floor(bc.x / 30)*30
-    bc.y = Math.floor(bc.y / 30)*30 
+    console.log(bc)
+    checkCube();
+    
+    bc.x = Math.floor(bc.x / 30) * 30;
+    bc.y = Math.floor(bc.y / 30) * 30;
     findSpotBigCube(bc.x, bc.y);
-  } //end onrelease 
-}//end CreateCube
-function findSpotBigCube(thisX, thisY){
-  //this function will find out where the 
+  }; //end onrelease
+} //end CreateCube
+
+function checkCube() {
+  /*this function gets called when the user lets go
+  of the shape.
+  The function will check if the shape is off the grid
+  */
+  
+  if (bc.x < 30) {
+    bc.x = 30;
+  }
+  if (bc.y < 30) {
+    bc.y = 30;
+  }
+  if (bc.x > 270) {
+    
+    bc.x = 240;
+  }
+  if (bc.y > 270) {
+    bc.y = 240;
+  }
+}
+function findSpotBigCube(thisX, thisY) {
+  //this function will find out where the
   //block got dropped
-  for(j=0 ; j< 10 ; j++){
-    for(i=0 ; i<10 ; i++){
-      if(grid[j][i].x == thisX && grid[j][i].y == thisY){
+  for (j = 0; j < 10; j++) {
+    for (i = 0; i < 10; i++) {
+      if (grid[j][i].x == thisX && grid[j][i].y == thisY) {
         grid[j][i].color = "yellow";
-        grid[j+1][i].color = "yellow";
-        grid[j][i+1].color = "yellow";
-        grid[j+1][i+1].color = "yellow";
-        grid[j+1][i+2].color = "yellow";
-        grid[j+2][i+1].color = "yellow";
-        grid[j+2][i].color = "yellow";
-        grid[j+2][i+2].color = "yellow";
-        grid[j][i+2].color = "yellow";
-      }// end if
-    }//end i loop
-  }//end j loop
-  bc.x = 500
-}//end find spot
+        grid[j + 1][i].color = "yellow";
+        grid[j][i + 1].color = "yellow";
+        grid[j + 1][i + 1].color = "yellow";
+        grid[j + 1][i + 2].color = "yellow";
+        grid[j + 2][i + 1].color = "yellow";
+        grid[j + 2][i].color = "yellow";
+        grid[j + 2][i + 2].color = "yellow";
+        grid[j][i + 2].color = "yellow";
+      } // end if
+    } //end i loop
+  } //end j loop
+  bc.x = 500;
+} //end find spot
